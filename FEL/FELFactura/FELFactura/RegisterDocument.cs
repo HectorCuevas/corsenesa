af58760c8636333e6c84f4039e36ff928556bccd
hectorcuevas <hcuevas@prosisco.com.gt>
2021-07-12 12:54:43 -0600

    initial commit

---
+++ FEL/FELFactura/FELFactura/RegisterDocument.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net;
+using System.Xml;
+using System.Text;
+using System.IO;
+using System.Data;
+namespace FELFactura
+{
+    public class RegisterDocument
+    {
+
+
+        public XmlDocument registerDte(String token, String dataXml, String url)
+        {
+            //ENVIANDO DOCUMENTO
+            var request = (HttpWebRequest)WebRequest.Create(url + Constants.URL_REGISTRAR_DOCUMENTO);
+            var postData = getPostData(dataXml);
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(postData.ToString());
+            var data = Encoding.UTF8.GetBytes(xmlDoc.InnerXml);
+            request.Headers.Add("authorization", "Bearer " + token.ToString().Trim());
+            request.Method = "POST";
+            request.ContentType = "application/xml";
+            request.ContentLength = data.Length;
+
+            using (var stream = request.GetRequestStream())
+            {
+                stream.Write(data, 0, data.Length);
+            }
+
+
+            var response = (HttpWebResponse)request.GetResponse();
+            string responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+            XmlDocument xmlDoc2 = new XmlDocument();
+            xmlDoc2.LoadXml(responseString);
+            return xmlDoc2;
+
+
+
+
+        }
+
+        private String getPostData(String GTDocumento)
+        {
+
+
+            string uuid = Guid.NewGuid().ToString().ToUpper();
+
+            String request = "<?xml version='1.0' encoding='UTF-8'?>\n" +
+              "<RegistraDocumentoXMLRequest id=\"" + uuid + "\">\n" +
+                        "<xml_dte>" +
+                            " <![CDATA[" + GTDocumento + "]]>\n" +
+                            "</xml_dte>\n" +
+                            "</RegistraDocumentoXMLRequest>";
+
+            //DateTime aDate = DateTime.Now;
+            //XmlDocument doc = new XmlDocument();
+            //doc.PreserveWhitespace = true;
+            //doc.LoadXml(request);
+            //String path = "C:\\xml\\FACT\\abc";// + aDate.ToString("dddd, dd MMMM yyyy HH:mm:ss");
+            //using (XmlTextWriter writer = new XmlTextWriter(path+".xml", null))
+            //{
+            //    writer.Formatting = System.Xml.Formatting.Indented;
+            //    doc.Save(writer);
+            //}
+
+            return request;
+        }
+    }
+}
---
+++ FEL/FELFactura/FELFactura/RegisterDocumentWS.asmx.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using System.Web.Services;
+using System.Xml;
+namespace FELFactura
+{
+    /// <summary>
+    /// Descripción breve de RegisterDocumentWS
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class RegisterDocumentWS : System.Web.Services.WebService
+    {
+        RegisterDocument ws = new RegisterDocument();
+        ValidateDocument wsvalidate = new ValidateDocument();
+        IDocumentRegister xml = null;
+        DataSet strreponsexml = new DataSet();
+
+        [WebMethod]
+        public DataSet registerDocument(String token, String XMLInvoice, String XMLDetailInvoce, String path, String fac_num, String url, string tipo)
+        {
+            try
+            {
+
+                //VALIDAR QUE NO ESTEN VACIOS LOS  DATOS ENVIADOS
+                if (!validateEmply(token, XMLInvoice, XMLDetailInvoce))
+                {
+                    return strreponsexml;
+                }
+
+
+                abstractFactory(tipo);
+                //TODO VALIDAR QUE  SI EL DOCUMENTO YA EXISTE SOLO DEVUELVA EL UUID
+
+                //SE ENVIA DATOS PARA QUE ARME LA ESTRUCTURA DE XML
+                String xmlDoc = xml.getXML(XMLInvoice, XMLDetailInvoce, path, fac_num);
+
+
+
+                //SE ENVIA XML PARA REGISTRAR DOCUMENTO
+                XmlDocument register = ws.registerDte(token, xmlDoc, url);
+
+                //SE VALIDA RESPUESTA DEL SERVICIO
+                XmlNodeList resReg = register.GetElementsByTagName("tipo_respuesta");
+                string errorRes = resReg[0].InnerXml;
+
+                // SI EL SERVICIO RETORNA ERROR SE ARMA LA ESTRUCTURA PARA RESPONDER LOS ERRORES A PROFIT
+                if ("1".Equals(errorRes.ToString()))
+                {
+
+                    String errorDescp = getError(register);
+                    strreponsexml = GetResponseXML(errorDescp, errorRes, strreponsexml);
+                    return strreponsexml;
+                }
+
+                //SI EL SERVICIO FUE RETORNA EXITOSO RETORNA UUID GENERADO POR EL FIRMADO ELECTRONICO
+                XmlNodeList uuidNodo = register.GetElementsByTagName("uuid");
+                string uuid = uuidNodo[0].InnerXml;
+                strreponsexml = GetResponseXML("Transacción Exitosa", uuid, errorRes, strreponsexml);
+                return strreponsexml;
+            }
+            catch (Exception ex)
+            {
+                // this.strreponsexml = GetResponseXML(e.ToString(), "1", this.strreponsexml);
+                //  strreponsexml.Tables.Add(new DataTable());
+                // strreponsexml.Tables[0].Columns.Add("res", typeof(string));
+                DataSet dsError = new DataSet();
+                DataTable dt = new DataTable("resultado");
+                dt.Columns.Add(new DataColumn("resultado", typeof(string)));
+                //dt.Columns.Add(new DataColumn("xmlGenerado", typeof(string)));
+
+                DataRow dr = dt.NewRow();
+                dr["resultado"] = ex.ToString();
+                //  dr["xmlGenerado"] =xmlDoc;
+                dt.Rows.Add(dr);
+                dsError.Tables.Add(dt);
+                return dsError;
+
+            }
+        }
+
+        private IDocumentRegister abstractFactory(string tipo)
+        {
+            IDocumentRegister notReg = null;
+            switch (tipo)
+            {
+                case "FACT":
+                    xml = new XMLFactura();
+                    break;
+                case "FCAM":
+                    xml = new XMLFacturaCambiaria();
+                    break;
+                case "FACAM":
+                    xml = new XMLFacturaCambiaria();
+                    break;
+                default:
+                    xml = new XMLFactura();
+                    break;
+            }
+
+
+            return notReg;
+        }
+
+        private bool validateEmply(String token, String XMLInvoice, String XMLDetailInvoce)
+        {
+            try
+            {
+
+                if (string.IsNullOrEmpty(token))
+                {
+                    strreponsexml = GetResponseXML("Error, token no puede ser vacío", "1", strreponsexml);
+                    return false;
+                }
+
+                if (string.IsNullOrEmpty(XMLInvoice))
+                {
+                    strreponsexml = GetResponseXML("Error, Datos de Factura no han sido enviados ", "1", strreponsexml);
+                    return false;
+                }
+
+
+                if (string.IsNullOrEmpty(XMLDetailInvoce))
+                {
+                    strreponsexml = GetResponseXML("Error, Detalles de Factura no han sido enviados ", "1", strreponsexml);
+                    return false;
+                }
+
+
+            }
+            catch (Exception e)
+            {
+                strreponsexml = GetResponseXML("Funcion vacio \n " + e.ToString(), "1", strreponsexml);
+                return false;
+
+            }
+            return true;
+        }
+
+
+
+        private DataSet GetResponseXML(String valor, string errores, DataSet strreponsexml)
+        {
+            try
+            {
+                strreponsexml = new DataSet();
+
+                if (valor == null)
+                { valor = " "; }
+
+                XmlDocument xmlDoc = new XmlDocument();
+
+                XmlNode rootNode = xmlDoc.CreateElement("NewDataset");
+                xmlDoc.AppendChild(rootNode);
+
+                XmlNode xtable = xmlDoc.CreateElement("Table");
+                rootNode.AppendChild(xtable);
+
+                XmlNode xvalor = xmlDoc.CreateElement("respuesta");
+                if (valor != null && valor.Length > 0)
+                {
+                    xvalor.InnerText = valor.ToString();
+                }
+                xtable.AppendChild(xvalor);
+
+                XmlNode xerror = xmlDoc.CreateElement("blnerror");
+                xerror.InnerText = errores.ToString();
+                xtable.AppendChild(xerror);
+                StringWriter stringWriter = new StringWriter();
+                XmlTextWriter xmlTextWriter = new XmlTextWriter(stringWriter);
+                xmlDoc.WriteTo(xmlTextWriter);
+                StringReader reader = new StringReader(stringWriter.ToString());
+                strreponsexml.ReadXml(reader);
+
+            }
+            catch
+            {
+
+            }
+            return strreponsexml;
+        }
+
+        private DataSet GetResponseXML(String valor, String uuid, string errores, DataSet strreponsexml)
+        {
+            try
+            {
+
+                strreponsexml = new DataSet();
+                if (valor == null)
+                { valor = " "; }
+                if (uuid == null)
+                { uuid = " "; }
+
+                XmlDocument xmlDoc = new XmlDocument();
+                XmlNode rootNode = xmlDoc.CreateElement("NewDataset");
+                xmlDoc.AppendChild(rootNode);
+
+                XmlNode xtable = xmlDoc.CreateElement("Table");
+                rootNode.AppendChild(xtable);
+
+                XmlNode xvalor = xmlDoc.CreateElement("respuesta");
+                if (valor != null && valor.Length > 0)
+                {
+                    xvalor.InnerText = valor.ToString();
+                }
+                xtable.AppendChild(xvalor);
+
+                XmlNode xuuid = xmlDoc.CreateElement("uuid");
+                if (uuid != null && uuid.Length > 0)
+                {
+                    xuuid.InnerText = uuid.ToString();
+                }
+                xtable.AppendChild(xuuid);
+
+                XmlNode xerror = xmlDoc.CreateElement("blnerror");
+                xerror.InnerText = errores.ToString();
+                xtable.AppendChild(xerror);
+                StringWriter stringWriter = new StringWriter();
+                XmlTextWriter xmlTextWriter = new XmlTextWriter(stringWriter);
+                xmlDoc.WriteTo(xmlTextWriter);
+                StringReader reader = new StringReader(stringWriter.ToString());
+                strreponsexml.ReadXml(reader);
+
+            }
+            catch
+            {
+
+            }
+            return strreponsexml;
+        }
+
+        private String getError(XmlDocument doc)
+        {
+
+
+            XmlNode unEmpleado;
+            String errores = "";
+            XmlNodeList lst = doc.GetElementsByTagName("error");
+
+
+            int count = lst.Count;
+            for (int i = 0; i < count; i++)
+            {
+
+                unEmpleado = lst.Item(i);
+
+                string id = unEmpleado.SelectSingleNode("cod_error").InnerText;
+                string error = unEmpleado.SelectSingleNode("desc_error").InnerText;
+
+                errores += " Código: " + id + ", Error: " + error + "\n";
+
+            }
+            return errores;
+        }
+    }
+}
